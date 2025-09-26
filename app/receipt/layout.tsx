e037dabfed41dd2625a519ad626a2e0a5b8b4ea7
Adietya Christian <adietya.christian@ui.ac.id>
2025-09-26 19:49:46 +0700

    hotfix hydration error

--- app/receipt/layout.tsx
+++ app/receipt/layout.tsx
@@ -14,21 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    // <html lang="en">
-    //   <body className={`${roboto.className} antialiased`}>{children}</body>
-    // </html>
-    <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
-        />
-      </head>
-      <body>
-        <div id="root">{children}</div>
-        <div id="drawer-root" />
-        <Toaster />
-      </body>
-    </html>
+    <>
+      <div id="root">{children}</div>
+      <div id="drawer-root" />
+      <Toaster />
+    </>
   );
 }
